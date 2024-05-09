063cc9707d6e25abe13e638e5bdd22d48e3df0be
Ashley Davis <ashley@codecapers.com.au>
2024-05-09 13:03:32 +1000

    Added code and registration for the service worker.

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-console.log(`Hello world!`);
+
+if (navigator && navigator.serviceWorker) {
+    navigator.serviceWorker.register('service-worker.js')
+        .then(registration => {
+            console.log(`Service Worker registered! Scope: ${registration.scope}`);
+            console.log(registration);
+        })
+        .catch(err => {
+            console.error(`Failed to register the service worker:`);
+            console.error(err);
+        });
+}
