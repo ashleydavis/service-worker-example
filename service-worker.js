7e36edba27f2c9a01e1643d77b258021d30a9df5
Ashley Davis <ashley@codecapers.com.au>
2024-05-09 15:58:47 +1000

    Added a network-first caching strategy.

--- service-worker.js
+++ service-worker.js
@@ -1,35 +1,52 @@
 
 console.log(`Hello from the service worker!`);
 
-//
-// On install, precache the resources for this web page.
-//
-self.addEventListener("install", event => {
+const CACHE_NAME = "app-v1";
 
-    console.log(`Precaching resources for the web page.`);
+//
+// Pre-caches the resources for this web page.
+//
+function preCache() {
+    //
+    // On install, precache the resources for this web page.
+    //
+    self.addEventListener("install", event => {
+    
+        console.log(`Precaching resources for the web page.`);
+    
+        event.waitUntil(addResourcesToCache([
+            "/",
+            "/index.js",
+        ]));
+    });
+}
 
-    event.waitUntil(addResourcesToCache([
-        "/",
-        "/index.js",
-    ]));
-});
+//
+// Pre-cache the resources for this web page.
+//
+// preCache();
 
 //
 // On fetch, return the cached resources.
 //
 self.addEventListener("fetch", event => {
 
-    event.respondWith(cacheFirst(event.request));
+    console.log(`Service worker ${CACHE_NAME}`);
+    console.log(event);
+
+    // event.respondWith(cacheFirst(event.request));
+
+    event.respondWith(networkFirst(event.request));
 });
 
 //
 // Puts a response in the cache.
 //
 async function cacheResponse(request, response) {
-    const cache = await caches.open("v1");
+    const cache = await caches.open(CACHE_NAME);
     await cache.put(request, response);
 
-    console.error(`Cached response for HTTP ${request.method} ${request.url}`);
+    console.log(`Cached response for HTTP ${request.method} ${request.url}`);
 };
 
 //
@@ -80,9 +97,63 @@ async function cacheFirst(request) {
 };
 
 //
+// Fetch using a network-first strategy.
+//
+async function networkFirst(request) {
+    try {
+        //
+        // Fetch the resource from the network.
+        //
+        console.log(`>> HTTP ${request.method} ${request.url} from network.`);
+        const responseFromNetwork = await fetch(request);
+
+        if (request.scheme === "http" || request.scheme === "https") {
+            //
+            // Cache the response.
+            //
+            // Cloning the response is necessary because request and response streams can only be read once.
+            //
+            cacheResponse(request, responseFromNetwork.clone())
+                .catch(err => {
+                    console.error(`Failed to cache response for HTTP ${request.method} ${request.url}`);
+                    console.error(err);
+                });
+        }
+
+        return responseFromNetwork;
+    }
+    catch (err) {
+        console.error(`Failed to fetch Request ${request.method} ${request.url} from network.`);
+        console.error(err);
+
+        //
+        // Fetching the resource from the network failed. Try to get it from the cache.
+        //
+        console.log(`>> Request ${request.method} ${request.url} from cache.`);
+        const responseFromCache = await caches.match(request);
+        if (responseFromCache) {
+            //
+            // Request is satsified from the cache.
+            //
+            console.log(`Requuest satisfied from cache: Request ${request.method} ${request.url}`);
+            return responseFromCache;
+        }
+
+        //
+        // There is nothing in the cache and a network error happened.
+        //
+        const message = `An error happened while fetching Request ${request.method} ${request.url}]\r\n${err}`;
+        return new Response(message, {
+            status: 408,
+            headers: { "Content-Type": "text/plain" },
+        });        
+    }
+}
+
+//
 // Caches a list of resources.
 //
 async function addResourcesToCache(resources) {
-    const cache = await caches.open("v1");
+    const cache = await caches.open(CACHE_NAME);
     await cache.addAll(resources);
 };
